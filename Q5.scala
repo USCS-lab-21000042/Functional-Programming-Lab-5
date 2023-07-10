11185b3e27a6035e6fd34c475bb139abe9d34f84
visal <visaluni2@gmail.com>
2023-07-10 22:35:30 +0530

    Q5

---
+++ Q5.scala
@@ -0,0 +1,20 @@
+object test extends App{
+    def checknumber(x:Int,d:Int=1):Boolean={
+        if(x==d ) false
+        else if(x<d) true
+        else checknumber(x,d+2)
+    }
+    
+    def totalofeven( x:Int):Int={
+        if(checknumber(x) && x>1){
+            
+            x-2+totalofeven(x-2)
+        }
+        else if(x>1 && !checknumber(x)) {
+            
+            (x-1)+totalofeven(x-1)
+        }
+        else 0
+    }
+    println(totalofeven(11))
+}
