7975742ab150629b27224bc124800b2d76d7afa8
visal <visaluni2@gmail.com>
2023-07-11 08:45:21 +0530

    Q4

--- Q4.scala
+++ Q4.scala
@@ -1,7 +1,7 @@
 object test extends App{
     def checknumber(x:Int,d:Int=1):Unit={
-        if(x==d ) println("Odd number")
-        else if(x<d) println("Even number")
+        if(x==d ) println(s"${x} is an Odd number")
+        else if(x<d) println(s"${x} is an Even number")
         else checknumber(x,d+2)
     }
     checknumber(5)
